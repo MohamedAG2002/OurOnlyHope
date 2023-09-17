106db03cdc0f86a498d91247673a59d6d6b47758
MohamedA.G2002 <moonimooni132@gmail.com>
2023-09-18 02:27:42 +0300

    WHY ARE YOU SO HARD GITgit add -A

--- CMakeFiles/ooh.dir/src/Components/Transform2D.cpp.o.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/ooh.dir/src/Components/Transform2D.cpp.o: \
- /home/mohamed/Dev/C++/Raylib/OurOnlyHope/src/Components/Transform2D.cpp \
- /usr/include/stdc-predef.h \
- /home/mohamed/Dev/C++/Raylib/OurOnlyHope/src/Components/Transform2D.hpp \
- /home/mohamed/Dev/C++/Raylib/OurOnlyHope/libs/include/raylib.h \
- /usr/lib/gcc/x86_64-linux-gnu/11/include/stdarg.h
--- CMakeFiles/ooh.dir/src/Entities/Zombie.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/ooh.dir/src/Entities/Zombie.cpp.o: \
- /home/mohamed/Dev/C++/Raylib/OurOnlyHope/src/Entities/Zombie.cpp \
- /usr/include/stdc-predef.h
